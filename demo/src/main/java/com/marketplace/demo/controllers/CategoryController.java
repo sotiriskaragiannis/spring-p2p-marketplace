7cdd463daf1dfdc54f732c7e65b4205918442541
sotiriskaragiannis <skaragian03@gmail.com>
2025-04-14 16:05:44 +0300

    removed unnecessary imports

--- demo/src/main/java/com/marketplace/demo/controllers/CategoryController.java
+++ demo/src/main/java/com/marketplace/demo/controllers/CategoryController.java
@@ -1,7 +1,6 @@
 package com.marketplace.demo.controllers;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marketplace.demo.models.Category;
-import com.marketplace.demo.models.Item;
-import com.marketplace.demo.models.dto.ItemStripped;
 import com.marketplace.demo.services.CategoryService;
 
 @RestController
