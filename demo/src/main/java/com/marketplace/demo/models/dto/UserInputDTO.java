016ba1785346b6691481c96c54bdaa8f5b0bd8f5
sotiriskaragiannis <skaragian03@gmail.com>
2025-05-22 21:13:55 +0300

    added UserInputDTO Class

---
+++ demo/src/main/java/com/marketplace/demo/models/dto/UserInputDTO.java
@@ -0,0 +1,88 @@
+package com.marketplace.demo.models.dto;
+
+public class UserInputDTO {
+
+	private String id;
+	private String username;
+	private String password;
+	private String full_name;
+	private String email;
+	private String bio;
+	private String country;
+	private String city;
+	private String phone_number;
+	
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getFull_name() {
+		return full_name;
+	}
+	
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getBio() {
+		return bio;
+	}
+	
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getPhone_number() {
+		return phone_number;
+	}
+	
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	
+}
